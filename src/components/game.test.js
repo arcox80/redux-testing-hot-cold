23d2855bfddd0203acb396c94fe093799d3c094a
Andrew Cox <drew.r.cox@gmail.com>
2017-10-19 13:57:46 -0400

    Add tests for header component

--- src/components/game.test.js
+++ src/components/game.test.js
@@ -11,18 +11,22 @@ describe('<Game />', () => {
   it('Renders without crashing', () => {
     shallow(<Game />);
   });
+  
   it('Renders Header initially', () => {
     const wrapper = shallow(<Game />);
     expect(wrapper.containsMatchingElement(<Header />)).toEqual(true);
   });
+  
   it('Renders GuessSection initially', () => {
     const wrapper = shallow(<Game />);
     expect(wrapper.containsMatchingElement(<GuessSection />)).toEqual(true);
   });
+  
   it('Renders GuessCount initially', () => {
     const wrapper = shallow(<Game />);
     expect(wrapper.containsMatchingElement(<GuessCount />)).toEqual(true);
   });
+  
   it('Renders GuessList initially', () => {
     const wrapper = shallow(<Game />);
     expect(wrapper.containsMatchingElement(<GuessList />)).toEqual(true);
---
+++ src/components/header.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import {Header} from './header.js';
+import TopNav from './top-nav';
+import InfoModal from './info-modal';
+
+describe('<Header />', () => {
+  it('Renders without crashing', () => {
+      shallow(<Header />);
+  });
+  
+  it('Renders TopNav initially', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.containsMatchingElement(<TopNav />)).toEqual(true);
+  });
+  
+  it('Should not render the info modal', () => {
+    const wrapper = shallow(<Header showInfoModal={false} />);
+    expect(wrapper.find(InfoModal).exists()).toEqual(false);
+  });
+  
+  it('Should render the info modal', () => {
+    const wrapper = shallow(<Header showInfoModal={true} />);
+    expect(wrapper.find(InfoModal).exists()).toEqual(true);
+  });
+});
