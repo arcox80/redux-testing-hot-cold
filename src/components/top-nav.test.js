92ba6426d235e3d0b2e20684a75952db36d40f29
Andrew Cox <drew.r.cox@gmail.com>
2017-10-19 17:40:41 -0400

    Add tests for top-nav component

---
+++ src/components/top-nav.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import {TopNav} from './top-nav';
+import {NEW_GAME, toggleInfoModal} from '../actions';
+
+describe('<TopNav />', () => {
+    it('Renders without crashing', () => {
+        shallow(<TopNav />);
+    });
+    
+    it('Should dispatch toggleInfoModal when What is clicked', () => {
+      const dispatch = jest.fn();
+      const wrapper = shallow(<TopNav dispatch={dispatch} />);
+      wrapper.find('.what').simulate('click', { preventDefault: () => {} });
+      expect(dispatch).toHaveBeenCalled();
+      expect(dispatch.mock.calls[0][0]).toEqual(toggleInfoModal());      
+    });
+
+    it('Should dispatch newGame when New Game is clicked', () => {
+      const dispatch = jest.fn();
+      const wrapper = shallow(<TopNav dispatch={dispatch} />);
+      wrapper.find('.new').simulate('click', { preventDefault: () => {} });
+      expect(dispatch).toHaveBeenCalled();
+      expect(dispatch.mock.calls[0][0].type).toEqual(NEW_GAME);      
+    });
+});
