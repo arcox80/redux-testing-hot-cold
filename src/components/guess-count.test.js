6dce5c0f413243719dfcdc77db1358017500d477
Andrew Cox <drew.r.cox@gmail.com>
2017-10-19 17:45:08 -0400

    Add tests for guess-count component

---
+++ src/components/guess-count.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import {GuessCount} from './guess-count.js';
+
+describe('<GuessCount />', () => {
+    it('Renders without crashing', () => {
+        shallow(<GuessCount />);
+    });
+    it('Renders the guess count correctly', () => {
+      const guessCount = 3;
+      const wrapper = shallow(<GuessCount count={guessCount} />);
+      expect(wrapper.text()).toEqual(`Guess #${guessCount}!`);
+  });
+});
