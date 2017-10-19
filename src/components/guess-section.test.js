1609dae4b2e654621c60277cd316ad44425aa1cc
Andrew Cox <drew.r.cox@gmail.com>
2017-10-19 17:26:00 -0400

    Add tests for guess-form component but still getting error

---
+++ src/components/guess-section.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import {GuessSection} from './guess-section.js';
+import GuessForm from './guess-form.js';
+
+describe('<GuessSection />', () => {
+    it('Renders without crashing', () => {
+        shallow(<GuessSection />);
+    });
+    it('Renders the Feedback', () => {
+      const feedback = 'Make your guess!';
+      const wrapper = shallow(<GuessSection feedback={feedback} />);
+      expect(wrapper.contains(feedback)).toEqual(true);
+    });
+    it('Renders GuessForm initially', () => {
+      const wrapper = shallow(<GuessSection />);
+      expect(wrapper.containsMatchingElement(<GuessForm />)).toEqual(true);
+    });
+});
